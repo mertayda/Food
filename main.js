dee2afc278fce76a6fdbd2af7ea8faa6981cce91
mertayda <48617781+mertayda@users.noreply.github.com>
2025-05-06 14:38:15 +0300

    sssd

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
 const searchButton = document.querySelector(".search-button")
 const formSearch = document.querySelector(".form-search")
-const carousel = document.querySelector(".seller-carousel")
-const carouselSlider = document.querySelector(".carousel-slider")
-const nextButton = document.querySelector(".next")
-const prevButton = document.querySelector(".prev")
+const navLinks = document.querySelector(".nav-links")
 
 
-searchButton.addEventListener("click", function() {
-    formSearch.classList.toggle("active")
-})
+document.addEventListener("DOMContentLoaded", function () {
+    const menuNavLiks = document.querySelectorAll(".menu-nav-link")
+    const categoryContent = document.querySelectorAll(".category-content")
 
+   
+    
+    
+    searchButton.addEventListener("click", function(e) {
+        e.preventDefault()
+        formSearch.classList.toggle("open")
+    })
+    
+    
+    
+    menuNavLiks.forEach((link) => {
+        link.addEventListener("click", function(e){
+            e.preventDefault()
+    
+            menuNavLiks.forEach((I) => {
+                I.classList.remove("active")
+            })
+    
+            categoryContent.forEach((content) =>{
+                content.classList.remove("active")
+            })
+    
+            this.classList.add("active")
+            const kategori = this.getAttribute("data-category")
+            const menukategori = document.getElementById(kategori)
+    
+    
+            if(menukategori){
+                menukategori.classList.add("active")
+            }
+    
+    
+        })
+    })
+    
+    
+    
+    
+    
+    })
 
-let currentIndex = 0
-const totalIndex = carousel.length - 1
-const slidesTotal = 3
-const  remaingIndex = totalIndex - slidesTotal
-
-function translate(){
-    carouselSlider.style.transform = `translateX(-${currentIndex * 33.333}%)` 
-}
-
-
-nextButton.addEventListener("click", function() {
-   if(currentIndex < totalIndex){
-    currentIndex++
-
-    translate()
-   }
-})
-
-prevButton.addEventListener("click", function() {
-    if(currentIndex > 0){
-        currentIndex--
-
-        translate()
-    }
-})
-
-
-
-
-// Carousel making
-
-// 1) first decide how many cards 
-// 2) add buttons when you cicck the button it will translate 
-// 3)carousel mantığı bir sayaç ayarlıar ve bu sayaç butona bastığında arttığında kartı sağa veya çekiyor
+    
